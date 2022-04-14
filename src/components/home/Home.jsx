5837746ff84bf03feba9994da283ac86d8f1f1c5
clebson.araujo <clebson.araujo@l5.com.br>
2022-04-14 19:50:01 -0300

    Componente Home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Início" subtitle="Alice Andrade Fotografia">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">
+      Gerenciador de clientes e serviços de fotografia ByEmotion
+    </p>
+  </Main>
+);
