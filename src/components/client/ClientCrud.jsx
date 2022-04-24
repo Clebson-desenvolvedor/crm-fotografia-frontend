f54dd34e605fec532e072952257609f4812a0602
clebson.araujo <clebson.araujo@l5.com.br>
2022-04-23 23:30:54 -0300

    Criação tabela clientes

--- src/components/client/ClientCrud.jsx
+++ src/components/client/ClientCrud.jsx
@@ -18,12 +18,40 @@ export default class ClientCrud extends Component {
     this.setState({ stClients: responseClients.data });
   }
 
+  renderTable() {
+    const {stClients} = this.state;
+    return (
+      <div>
+        {console.log(stClients)}
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NOME</th>
+              <th scope="col">E-MAIL</th>
+              <th scope="col">WHATSAPP</th>
+            </tr>
+          </thead>
+          <tbody>
+           {stClients.map(e => (
+             <tr>
+               <td>{e.idcliente}</td>
+               <td>{e.nomecliente}</td>
+               <td>{e.email}</td>
+               <td>{e.whatsapp}</td>
+             </tr>
+           ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
   render() {
-    const { stClients } = this.state;
     return (
       <Main {...headerProps}>
-        Lista de Clientes
-       
+        <h3>Lista de Clientes</h3>
+        {this.renderTable()}
       </Main>
     );
   }
