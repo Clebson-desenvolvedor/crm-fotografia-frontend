eb3cf09af8908b76dd2f510795ec1c795e489414
clebson.araujo <clebson.araujo@l5.com.br>
2022-07-17 18:49:30 -0300

    commit 170722

--- src/components/client/NewClientCrud.jsx
+++ src/components/client/NewClientCrud.jsx
@@ -1,69 +1,83 @@
-import React, { useEffect, useState } from "react";
+import { React, useState } from "react";
 import Main from "../template/Main";
-import { useForm } from "react-hook-form";
 import { api } from "../../backend/api";
+
 
 const headerProps = {
   icon: "user",
   title: "Crie um novo cliente",
 };
 
+
+
 export default function NewClientCrud() {
-  const { formData, setFormData } = useState("")
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    api.post("/clients", {
-      nomecliente: data.name,
-      dtnasccliente: data.birthday,
-      dtcadcliente: data.rDate,
-      email: data.email,
-      whatsapp: data.whatsapp,
-      enderecocliente: data.pPlace,
-      numeroendcliente: data.number,
-      bairrocliente: data.district,
-      cepcliente: data.cep,
-      cidadecliente: data.city,
-    }).then((response) => {
-      alert(response.data['mensagem']);
-      window.location.reload();
-    }).catch((error) => {
-      alert(error.response.data['error'])
-    });
+
+  const initialValue = {
+    name: '',
+    birthday: '',
+    rDate: '',
+    email: '',
+    whatsapp: '',
+    pPlace: '',
+    number: '',
+    district: '',
+    cep: '',
+    city: '',
+  }
+  const [values, setValues] = useState({initialValue});
+  function onChange(ev) {
+    const {name, value} = ev.target;
+    console.log(values)
+    setValues({ ...values, [name]: value});
+  }
+
+  function onSubmit(ev) {
+    ev.preventDefault();
+    api.post('/clients', values)
+      .then(response => {
+        alert(response.data['mensagem']);
+        window.location.reload();
+      }).catch((error) => {
+        alert(error.response.data['error']);
+      })
   }
 
   return (
     <Main {...headerProps}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={onSubmit}>
         <div class="form-row">
           <div class="form-group col-md-4">
             <label for="name">Nome</label>
-            <input {...register("name")}
+            <input
               type="text"
               class="form-control"
               id="name"
               placeholder="Digite o nome"
+              onChange={onChange}
             ></input>
           </div>
           <div class="form-group col-md-3">
             <label for="email">E-mail</label>
-            <input {...register("email")}
+            <input 
               type="email"
               class="form-control"
               id="email"
               placeholder="exemplo@email.com"
+              onChange={onChange}
             ></input>
           </div>
           <div class="form-group col-md-2">
             <label for="date">Data de Nascimento</label>
-            <input {...register("birthday")} type="date" class="form-control" id="birthday"></input>
+            <input type="date" class="form-control" id="birthday" onChange={onChange}></input>
           </div>
           <div class="form-group col-md-3">
             <label for="whatsapp">WhatsApp</label>
-            <input {...register("whatsapp")}
+            <input
               type="text"
               class="form-control"
               id="whatsapp"
               placeholder="(  ) xxxxx-xxxx"
+              onChange={onChange}
             ></input>
           </div>
         </div>
@@ -71,41 +85,42 @@ export default function NewClientCrud() {
         <div className="form-row">
           <div class="form-group col-md-2">
             <label for="cep">CEP</label>
-            <input {...register("cep")}
-              type="text"
+            <input type="text"
               class="form-control"
               id="cep"
               placeholder="00000-000"
+              onChange={onChange}
             ></input>
           </div>
 
           <div class="form-group col-md-4">
             <label for="rua">Logradouro</label>
-            <input {...register("pPlace")}
+            <input 
               class="form-control"
               type="text"
               id="pPlace"
               placeholder="rua, avenida, etc"
+              onChange={onChange}
             />
           </div>
           <div class="form-group col-md-1">
             <label for="numero">Número</label>
-            <input {...register("number")} type="text" class="form-control" id="number"></input>
+            <input type="text" class="form-control" id="number" onChange={onChange}></input>
           </div>
           <div class="form-group col-md-3">
             <label for="bairro">Bairro</label>
-            <input {...register("district")} type="text" class="form-control" id="district"></input>
+            <input type="text" class="form-control" id="district" onChange={onChange}></input>
           </div>
           <div class="form-group col-md-2">
             <label for="cidade">Cidade</label>
-            <input {...register("city")} type="text" class="form-control" id="city"></input>
+            <input type="text" class="form-control" id="city" onChange={onChange}></input>
           </div>
         </div>
 
         <div className="form-row">
           <div class="form-group col-md-2">
             <label for="datacad">Data de Cadastro do Cliente</label>
-            <input {...register("rDate")} type="date" class="form-control" id="rDate"></input>
+            <input  type="date" class="form-control" id="rDate" onChange={onChange}></input>
           </div>
         </div>
         <button type="submit" class="btn btn-success">
